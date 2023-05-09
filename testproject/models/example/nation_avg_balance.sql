e77e489dba5ff995713e6d8d53b1ffc4aedf6866
Michiel De Muynck <michiel.demuynck@ond.vlaanderen.be>
2023-05-09 15:46:45 +0200

    Remove more unnecessary files

--- testproject/models/example/nation_avg_balance.sql
+++
@@ -1,5 +0,0 @@
-select n_name, avg(c_acctbal) as avg_balance
-from {{ source('tpch', 'nation') }} nation
-join {{ source('tpch', 'customer') }} customer
-on nation.n_nationkey = customer.c_nationkey
-group by nation.n_name
